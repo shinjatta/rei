d972466caa06e98ac778f5aee2b12430330acdf6
Shinta <65914727+shinjatta@users.noreply.github.com>
2021-03-03 17:27:56 +0100

    intent de connectar amb la api pero no em surt

--- src/app/definicion/definicion.component.ts
+++ src/app/definicion/definicion.component.ts
@@ -14,11 +14,42 @@ export class DefinicionComponent implements OnInit {
   traduccionIngles="inheritance, succession";
   traduccionEspañol="herencia, sucesión";
   subscription!: Subscription;
+
+ /*  JishoApi = require('unofficial-jisho-api');
+  jisho = new this.JishoApi();
+  proxy = 'https://cors-anywhere.herokuapp.com/'
+  url = this.proxy + this.jisho.getUriForPhraseSearch(this.palabraPrincipal);
+
+  response = await fetch(this.url);
+  json = await this.response.json(); */
+  
+/* 
+  // Listen on a specific host via the HOST environment variable
+  var host = process.env.HOST || '0.0.0.0';
+  // Listen on a specific port via the PORT environment variable
+  var port = process.env.PORT || 8080;
+
+  var cors_proxy = require('cors-anywhere');
+  cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+  }).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+  }); */
   
   constructor(private buscadorComponent: BuscadorComponent, private data: DataService) { }
 
   ngOnInit(): void {
-    this.subscription = this.data.currentSearch.subscribe(search => this.palabraPrincipal = search)
+    /* this.subscription = this.data.currentSearch.subscribe(search => this.palabraPrincipal = search)
+    console.log(this.json); */
+
+
+   /*  this.jisho.searchForPhrase(this.palabraPrincipal).then((result: any) => {
+      this.traduccionIngles=result.english;
+      console.log(this.traduccionIngles);
+    }); */
   }
 
+  
 }
